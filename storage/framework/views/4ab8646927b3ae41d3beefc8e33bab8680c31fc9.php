b280cd6aef2d75501b34d21c49a7bb5ac5ce27f0
AnastasiaSidorenko <snasty97@mail.ru>
2019-05-24 14:56:32 +0300

    24.05 14:56

--- storage/framework/views/4ab8646927b3ae41d3beefc8e33bab8680c31fc9.php
+++ storage/framework/views/4ab8646927b3ae41d3beefc8e33bab8680c31fc9.php
@@ -9,7 +9,7 @@
     <title><?php echo $__env->yieldContent('title'); ?></title>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
-
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
 
 <!-- Bootstrap core CSS -->
@@ -26,6 +26,12 @@
     <script type="text/javascript" src="<?php echo e(asset('js/bootstrap.min.js')); ?>"></script>
     <!-- MDB core JavaScript -->
     <script type="text/javascript" src="<?php echo e(asset('js/mdb.js')); ?>"></script>
+
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+    </script>
     <!-- Managers -->
     <?php echo $__env->yieldContent('js'); ?>
 
@@ -74,7 +80,7 @@
         <a href="manufacturers"><?php echo e(trans('admin.manufacturers')); ?></a>
     </div>
 </div>
-
+<?php echo $__env->make('common.errors', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 <?php echo $__env->yieldContent('content'); ?>
 
 </body>
