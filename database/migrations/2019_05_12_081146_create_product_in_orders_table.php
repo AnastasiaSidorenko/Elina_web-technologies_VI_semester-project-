7afd1f53b1a986e2d0b9095a443d32009454915a
tasha <snata97@mail.ru>
2019-05-27 20:47:10 +0300

    27may 20:38 2

--- database/migrations/2019_05_12_081146_create_product_in_orders_table.php
+++ database/migrations/2019_05_12_081146_create_product_in_orders_table.php
@@ -14,16 +14,16 @@ class CreateProductInOrdersTable extends Migration
     public function up()
     {
         Schema::create('product_in_orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->integer('quantity');
-            $table->bigInteger('id_order')->unsigned();
-            $table->bigInteger('id_product')->unsigned();
+            $table->bigInteger('id_order')->unsigned()->unique();
+            $table->bigInteger('id_product')->unsigned()->unique();
             $table->foreign('id_order')->references('id')->on('orders')
                 ->onDelete('cascade')
                 ->onUpdate('cascade');
             $table->foreign('id_product')->references('id')->on('products')
                 ->onDelete('cascade')
                 ->onUpdate('cascade');
-            $table->primary(array('id_order', 'id_product'));
             $table->timestamps();
         });
     }
--- database/migrations/2019_05_12_081317_create_feedback_on_products_table.php
+++ database/migrations/2019_05_12_081317_create_feedback_on_products_table.php
@@ -14,15 +14,15 @@ class CreateFeedbackOnProductsTable extends Migration
     public function up()
     {
         Schema::create('feedback_on_products', function (Blueprint $table) {
-            $table->bigInteger('id_product')->unsigned();
-            $table->bigInteger('id_feedback')->unsigned();
+            $table->bigIncrements('id');
+            $table->bigInteger('id_product')->unsigned()->unique();
+            $table->bigInteger('id_feedback')->unsigned()->unique();
             $table->foreign('id_product')->references('id')->on('products')
                 ->onDelete('cascade')
                 ->onUpdate('cascade');
             $table->foreign('id_feedback')->references('id')->on('feedbacks')
                 ->onDelete('cascade')
                 ->onUpdate('cascade');
-            $table->primary(array('id_product', 'id_feedback'));
             $table->timestamps();
         });
     }
